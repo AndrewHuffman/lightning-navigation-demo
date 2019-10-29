598d95f550819f405357a3d1526172f56fc56ce6
Andrew Huffman <andrew.huffman@salesforce.com>
2019-10-29 18:00:18 -0400

    Creating package.xml

---
+++ navdemo.bak/.sfdx/typings/lwc/sobjects/Mascot__c.d.ts
@@ -0,0 +1,76 @@
+declare module "@salesforce/schema/Mascot__c.Biography__c" {
+  const Biography__c:string;
+  export default Biography__c;
+}
+declare module "@salesforce/schema/Mascot__c.Contact__c" {
+  const Contact__c:any;
+  export default Contact__c;
+}
+declare module "@salesforce/schema/Mascot__c.Contact__r" {
+  const Contact__r:any;
+  export default Contact__r;
+}
+declare module "@salesforce/schema/Mascot__c.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/Mascot__c.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/Mascot__c.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/Mascot__c.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/Mascot__c.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
+declare module "@salesforce/schema/Mascot__c.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/Mascot__c.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/Mascot__c.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/Mascot__c.LastReferencedDate" {
+  const LastReferencedDate:any;
+  export default LastReferencedDate;
+}
+declare module "@salesforce/schema/Mascot__c.LastViewedDate" {
+  const LastViewedDate:any;
+  export default LastViewedDate;
+}
+declare module "@salesforce/schema/Mascot__c.Name" {
+  const Name:string;
+  export default Name;
+}
+declare module "@salesforce/schema/Mascot__c.Owner" {
+  const Owner:any;
+  export default Owner;
+}
+declare module "@salesforce/schema/Mascot__c.OwnerId" {
+  const OwnerId:any;
+  export default OwnerId;
+}
+declare module "@salesforce/schema/Mascot__c.Picture_Id__c" {
+  const Picture_Id__c:string;
+  export default Picture_Id__c;
+}
+declare module "@salesforce/schema/Mascot__c.Picture__c" {
+  const Picture__c:string;
+  export default Picture__c;
+}
+declare module "@salesforce/schema/Mascot__c.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
